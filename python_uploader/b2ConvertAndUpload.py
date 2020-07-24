3ccf3bc6046bd9dc4ac6a2057fcb21953a058844
Gijs <gijs@finestbit.com>
2020-07-24 22:41:23 +0200

    add upload to streama

--- python_uploader/b2ConvertAndUpload.py
+++ python_uploader/b2ConvertAndUpload.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from urllib.parse import quote
 import base64
 import json
 import subprocess ##pip3 install subprocess32
@@ -7,6 +8,11 @@ import re
 import sys
 import io
 import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 def Run(command):
     proc = subprocess.Popen(command,
@@ -77,9 +83,110 @@ def uploadToBackBlaze(convertedFile, b2path):
     print(process.stdout.read().decode('utf-8') + "\nUpload complete!")
 
 
+def addMovieToStreama(moviename,movieyear,movieurl):
+    driver = webdriver.Chrome()
+    driver.get("https://app.fireflix.stream/login/auth")
+    WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.XPATH, "//input[@placeholder='Username']")))
+    username = driver.find_element_by_xpath("//input[@placeholder='Username']")
+    password = driver.find_element_by_xpath("//input[@placeholder='Password']")
+    loginbutton = driver.find_element_by_xpath("//button[@class='btn btn-primary pull-right ng-binding']")
+    username.send_keys("gijs")
+    password.send_keys("896912666")
+    loginbutton.click()
+    WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.XPATH, "//a[contains(text(),'Manage Content')]")))
+    time.sleep(5)
+    managerbutton = driver.find_element_by_xpath("//a[contains(text(),'Manage Content')]")
+    managerbutton.click()
+    time.sleep(1)
+    managerbutton.click()
+    WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.XPATH, "//div[@class='nav']//a[contains(text(),'Movies')]")))
+    time.sleep(1)
+    moviebutton = driver.find_element_by_xpath("//div[@class='nav']//a[contains(text(),'Movies')]")
+    moviebutton.click()
+    WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.XPATH, "//input[@placeholder='Search Movie from collection or TheMovieDB...']")))
+    searchmoviefield = driver.find_element_by_xpath("//input[@placeholder='Search Movie from collection or TheMovieDB...']")
+    searchmoviefield.send_keys(moviename)
+    time.sleep(5)
+    foundmoviereleasedate = driver.find_element_by_xpath("//div[@class='media-list similar-media']//div[1]//div[3]")
+    if movieyear in foundmoviereleasedate.text:
+        print("Movie year matches: " + movieyear)
+        addmovieandopenbutton = driver.find_element_by_xpath("//div[@class='media-list similar-media']//div[1]//div[1]//div[1]//button[1]")
+        addmovieandopenbutton.click()
+        WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.XPATH, "//i[@class='ion-ios-settings icon-xl dropdown-toggle']")))
+        movieadddropdown = driver.find_element_by_xpath("//i[@class='ion-ios-settings icon-xl dropdown-toggle']")
+        movieadddropdown.click()
+        WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.XPATH, "//a[contains(text(),'Manage Files')]")))
+        managefilesbutton = driver.find_element_by_xpath("//a[contains(text(),'Manage Files')]")
+        managefilesbutton.click()
+        WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.XPATH, "//a[contains(text(),'External URL')]")))
+        externalurlbutton = driver.find_element_by_xpath("//a[contains(text(),'External URL')]")
+        externalurlbutton.click()
+        WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.XPATH, "//input[@placeholder='External URL']")))
+        urltextfield = driver.find_element_by_xpath("//input[@placeholder='External URL']")
+        urltextfield.send_keys(movieurl)
+        WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.XPATH, "//button[@class='btn btn-success btn-sm']")))
+        addurlbutton = driver.find_element_by_xpath("//button[@class='btn btn-success btn-sm']")
+        addurlbutton.click()
+        time.sleep(5)
+        driver.quit()
+    else:
+        print("The movie: " + moviename + ", release year: " + movieyear + " could not be found!")
+        print("Please add this movie manually.")
+        input("Press Enter to continue when this is done...")
+
+def addSerieToStreama(showname, seasonnumber, episodenumber, episodeurl):
+    driver = webdriver.Chrome()
+    driver.get("https://app.fireflix.stream/login/auth")
+    WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.XPATH, "//input[@placeholder='Username']")))
+    username = driver.find_element_by_xpath("//input[@placeholder='Username']")
+    password = driver.find_element_by_xpath("//input[@placeholder='Password']")
+    loginbutton = driver.find_element_by_xpath("//button[@class='btn btn-primary pull-right ng-binding']")
+    username.send_keys("gijs")
+    password.send_keys("896912666")
+    loginbutton.click()
+    WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.XPATH, "//a[contains(text(),'Manage Content')]")))
+    time.sleep(5)
+    managerbutton = driver.find_element_by_xpath("//a[contains(text(),'Manage Content')]")
+    managerbutton.click()
+    time.sleep(1)
+    managerbutton.click()
+    WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.XPATH, "//input[@placeholder='Search Show from collection or TheMovieDB...']")))
+    searchmoviefield = driver.find_element_by_xpath("//input[@placeholder='Search Show from collection or TheMovieDB...']")
+    searchmoviefield.send_keys(showname)
+    time.sleep(5)
+    addmovieandopenbutton = driver.find_element_by_xpath("//div[@class='media-list similar-media']//div[1]//div[1]//div[1]//button[1]")
+    addmovieandopenbutton.click()
+    time.sleep(2)
+    fetchepisodes = driver.find_element_by_xpath("//button[@class='btn btn-primary ng-scope']")
+    fetchepisodes.click()
+    time.sleep(2)
+    seasonnumberfield = driver.find_element_by_xpath("//input[@id='alertify-text']")
+    seasonnumberfield.send_keys(seasonnumber)
+    time.sleep(2)
+    fetchepisodesokbutton = driver.find_element_by_xpath("//button[@id='alertify-ok']")
+    fetchepisodesokbutton.click()
+    time.sleep(10)
+    mediacontainer = driver.find_element_by_xpath("//span[contains(text(),'"+seasonnumber+"e"+episodenumber+"')]")
+    parentElement = mediacontainer.find_element_by_xpath("..")
+    fullmediacontainer = parentElement.find_element_by_xpath("..")
+    editbutton = fullmediacontainer.find_element_by_xpath(".//button[contains(text(),'Manage Files')]")
+    editbutton.click()
+    WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.XPATH, "//a[contains(text(),'External URL')]")))
+    externalurlbutton = driver.find_element_by_xpath("//a[contains(text(),'External URL')]")
+    externalurlbutton.click()
+    WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.XPATH, "//input[@placeholder='External URL']")))
+    urltextfield = driver.find_element_by_xpath("//input[@placeholder='External URL']")
+    urltextfield.send_keys(episodeurl)
+    WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.XPATH, "//button[@class='btn btn-success btn-sm']")))
+    addurlbutton = driver.find_element_by_xpath("//button[@class='btn btn-success btn-sm']")
+    addurlbutton.click()
+    time.sleep(5)
+    driver.quit()
+    
+
 def mainMovie():
-    # createFileList("sftp_hetzner:/G:/films", "data/src_movielist.txt")
-    # createFileList("b2:devbucket735/Movies", "data/dest_movielist.txt")
+    createFileList("sftp_hetzner:/G:/films", "data/src_movielist.txt")
+    createFileList("b2:devbucket735/Movies", "data/dest_movielist.txt")
     dedupeFileLists("data/src_movielist.txt", "data/dest_movielist.txt", "data/parsedMovielist.txt")
     with open("data/parsedMovielist.txt",'r') as f:
         lines = f.readlines()
@@ -88,22 +195,24 @@ def mainMovie():
             oldfilepath=(line.rstrip()).split('/', 1)[-1]
             oldfilefolder=(line.rstrip()).split('/', 1)[0]
             
-            filepathmp4=os.path.splitext(oldfilepath)[0]+".mp4"
+            filepathmp4=os.path.splitext(oldfilefolder)[0]+".mp4"
             fullfilepathmp4="\'"+"/tmp/output/"+filepathmp4+"\'"
             b2mp4path="devbucket735/Movies/"+oldfilefolder
-            print(b2mp4path)
+            movieurl = "https://" + quote("cdn.fireflix.stream/file/"  + "devbucket735/Movies/"+oldfilefolder+"/"+oldfilefolder + ".mp4")
 
             downloadFileToTemp("sftp_hetzner:" + "\'" + "/G:/films/" + fullpath + "\'")
             proc = Run(["HandBrakeCLI","--preset-import-file","streama_handbrake.json","-Z","Streama","-i","/tmp/download/"+oldfilepath,"-o","/tmp/output/"+filepathmp4])
             Trace(proc)
             uploadToBackBlaze(fullfilepathmp4,"b2:" + "\'" + b2mp4path + "\'")
             os.remove("/tmp/download/"+oldfilepath)
+            print(oldfilefolder[:-6] + oldfilefolder[-5:-1] + movieurl)
+            addMovieToStreama(oldfilefolder[:-6], oldfilefolder[-5:-1], movieurl)
 
 
 def mainSerie():
     # createFileList("sftp_hetzner:/G:/amerikaanse series", "data/src_serielist.txt")
     # createFileList("b2:devbucket735/TV", "data/dest_serielist.txt")
-    dedupeFileLists("data/src_serielist.txt", "data/dest_serielist.txt", "data/parsedSerielist.txt")
+    # dedupeFileLists("data/src_serielist.txt", "data/dest_serielist.txt", "data/parsedSerielist.txt")
     with open("data/parsedSerielist.txt",'r') as f:
         lines = f.readlines()
         for line in lines:
@@ -115,11 +224,26 @@ def mainSerie():
             fullfilepathmp4="\'"+"/tmp/output/"+filepathmp4+"\'"
             b2mp4path="devbucket735/TV/"+oldfilefolder
 
+            episodeurl = "https://" + quote("cdn.fireflix.stream/file/"  + "devbucket735/TV/"+oldfilefolder+"/" + filepathmp4)
+            showname = oldfilefolder.split("/", 1)[0]
+            seasonnumber = ""
+            if "Specials" in oldfilefolder.split("/", 1)[1]: 
+                seasonnumber = "0" 
+            else: 
+                seasonnumber = oldfilefolder.split("/", 1)[1].split(" ", 1)[1]
+            episodenumber = re.findall(r"E[0-9]+",oldfilepath)
+            showname = showname.replace('(2019)', '') # fix another life show
+
             downloadFileToTemp("sftp_hetzner:" + "\'" + "/G:/amerikaanse series/" + fullpath + "\'")
             proc = Run(["HandBrakeCLI","--preset-import-file","streama_handbrake.json","-Z","Streama","-i","/tmp/download/"+oldfilepath,"-o","/tmp/output/"+filepathmp4])
             Trace(proc)
             uploadToBackBlaze(fullfilepathmp4,"b2:" + "\'" + b2mp4path + "\'")
             os.remove("/tmp/download/"+oldfilepath)
 
-mainMovie()
+            if len(episodenumber) > 1:
+                for epnum in range(int(episodenumber[0].split("E",1)[1]), int(episodenumber[1].split("E",1)[1]) + 1):
+                    addSerieToStreama(showname, seasonnumber, str(epnum), episodeurl)
+            else:
+                addSerieToStreama(showname, seasonnumber, episodenumber[0].split("E",1)[1], episodeurl)
 
+mainSerie()
