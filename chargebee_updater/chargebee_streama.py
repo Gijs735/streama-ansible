6a66d13c6eb84a1ed003e2f752c9ae827cb6177a
Gijs <gijs@finestbit.com>
2020-09-20 22:27:15 +0200

    update to fix crash

--- chargebee_updater/chargebee_streama.py
+++ chargebee_updater/chargebee_streama.py
@@ -1,20 +1,39 @@
 import chargebee
 import json
+import mysql.connector
 
-chargebee.configure("test_fcRPfdRIoanNgFbrPEGzbprua5Bb0g6r","fireflix-test")
-entries = chargebee.Subscription.list({
-    "limit" : 100,
-    "plan_id[in]" : ["standard"],
-    "status[in]" : ["active", "in_trial"]
-    })
-for entry in entries:
-  subscription = entry.subscription
-  customer = entry.customer
-  card = entry.card
+# chargebee.configure("test_fcRPfdRIoanNgFbrPEGzbprua5Bb0g6r","fireflix-test")
+# entries = chargebee.Subscription.list({
+#     "limit" : 100,
+#     "plan_id[in]" : ["standard"],
+#     "status[in]" : ["active", "in_trial", "non_renewing"]
+#     })
+# for entry in entries:
+#   subscription = entry.subscription
+#   customer = entry.customer
+#   card = entry.card
+#   print(customer.email)
 
-print(customer)
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  database="streama"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT username, account_locked FROM user")
+
+myresult = mycursor.fetchall()
+
+mycursor.close()
+
+for x in myresult:
+  print(x)
 
 
 #pip3 install 'chargebee>=2,<3'
 
 #https://apidocs.chargebee.com/docs/api/subscriptions#list_subscriptions_status
+#pip3 install mysql-connector-python
+#https://www.w3schools.com/python/python_mysql_select.asp
